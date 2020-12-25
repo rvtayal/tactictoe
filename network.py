54ad808a8d0c20187700f2ce3a9309b3572df6f7
RJ Tayal <rvtayal@gatech.edu>
2020-12-25 11:06:16 -0500

    bug fixes on network

--- network.py
+++ network.py
@@ -17,8 +17,11 @@ class ConvLayers(torch.nn.Module):
         self.batch_norm = torch.nn.BatchNorm2d(num_features=out_channels)
 
     def forward(self, state):
-        state = state.view(-1, self.in_channels, 8, 8)
-        return torch.nn.functional.relu(self.batch_norm(self.conv(state)))
+        state = state.view(-1, self.in_channels, 9, 9)
+        state = self.conv(state)
+        state = self.batch_norm(state)
+        state = torch.nn.functional.relu(state)
+        return state
 
 
 class ResLayers(torch.nn.Module):
@@ -45,14 +48,14 @@ class ResLayers(torch.nn.Module):
     def forward(self, x):
         out = torch.nn.functional.relu(self.batch_norm1(self.conv1(x)))
         out = self.batch_norm2(self.conv2(out)) + x
-        return torch.nn.function.relu(out)
+        return torch.nn.functional.relu(out)
 
 
 class OutLayers(torch.nn.Module):
     def __init__(self, in_channels=None):
-        super.__init__()
+        super().__init__()
         in_channels = 128 if in_channels is None else in_channels
-        self.conv_v torch.nn.Conv2d(in_channels=in_channels,
+        self.conv_v = torch.nn.Conv2d(in_channels=in_channels,
             out_channels=1,
             kernel_size=1)
         self.batch_norm_v = torch.nn.BatchNorm2d(1)
@@ -60,19 +63,24 @@ class OutLayers(torch.nn.Module):
         self.fc_v2 = torch.nn.Linear(64, 1)
 
         self.conv_p = torch.nn.Conv2d(in_channels=in_channels,
-            out_channels=out_channels,
+            out_channels=2,
             kernel_size=1)
         self.batch_norm_p = torch.nn.BatchNorm2d(2)
-        self.fc_p = torch.nn.Linear(128, 81)
+        self.fc_p = torch.nn.Linear(162, 81)
 
     def forward(self, x):
-        v = toch.nn.functional.relu(self.batch_norm_v(self.conv_v(x)))
+        v = torch.nn.functional.relu(self.batch_norm_v(self.conv_v(x)))
         v = v.view(-1, 81)
         v = torch.nn.functional.relu(self.fc_v1(v))
         v = torch.tanh(self.fc_v2(v))
 
-        p = torch.nn.functional.relu(self.batch_norm_p(self.conv_p(x)))
-        p = p.view(-1, 128)
+        print(x.shape)
+        p = self.conv_p(x)
+        print(p.shape)
+        p = torch.nn.functional.relu(self.batch_norm_p(p))
+        print(p.shape)
+        p = p.view(-1, 81*2)
+        print(p.shape)
         p = torch.nn.functional.softmax(self.fc_p(p), dim=1).flatten()
 
         return v, p
@@ -89,17 +97,19 @@ class TacticToeNet(torch.nn.Module):
         self.res_layers = torch.nn.Sequential(*[ResLayers() for _ in range(self.num_res_layers)])
         self.out_layers = OutLayers()
 
-    def forward(self, board):
-        x = self.conv_layers(TacticToeNet.encode_board(board))
+    def forward(self, game):
+        g = TacticToeNet.encode_game(game)
+        # print(g.shape)
+        x = self.conv_layers(g)
         x = self.res_layers(x)
         v,p = self.out_layers(x)
         return v.flatten(), p
 
 
     @staticmethod
-    def encode_board(game):
+    def encode_game(game):
         # def encode_board(board: LargeBoard) -> torch.tensor:
-        layers = torch.zeros((2,9,9), dtype=torch.float64)
+        layers = torch.zeros((2,9,9))
 
         board, pm = game.getState()
         # print(pm)
@@ -109,7 +119,7 @@ class TacticToeNet(torch.nn.Module):
             l1 *= -1
 
         # layer for pervious move
-        l2 = torch.zeros((9,9), dtype=torch.float64)
+        l2 = torch.zeros((9,9))
         if pm is not None:
             b, square = pm
             dc, dr = (3 * (b%3), 3 * (b//3))
@@ -126,7 +136,11 @@ def main():
     g = Game()
     g.move((2, (1,1)))
     g.move((4, (0,0)))
-    print(TacticToeNet.encode_board(g))
+    net = TacticToeNet(num_res_layers=5)
+    # net = net.float()
+    a,b = net(g)
+    print(a)
+    print(b)
 
 
 if __name__ == "__main__":
